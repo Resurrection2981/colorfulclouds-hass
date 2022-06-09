b9a23792f63b8198a164c701b9394f6cad4c28fb
Resurrection2981 <45873933+Resurrection2981@users.noreply.github.com>
2022-06-09 09:49:18 +0800

    Update __init__.py

--- custom_components/colorfulclouds/__init__.py
+++ custom_components/colorfulclouds/__init__.py
@@ -2,5 +2,5 @@
 Component to integrate with 彩云天气.
 
 For more details about this component, please refer to
-https://github.com/jihao/colorfulclouds-hass
+https://github.com/Resurrection2981/colorfulclouds-hass
 """
